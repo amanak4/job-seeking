1b533868cf5c723eb4c8015c6663c38b6ae97d9b
amanak7 <22je0094@iitism.ac.in>
2024-04-15 05:41:15 +0530

    intial2.o

--- backend/database/dbconnection.js
+++ backend/database/dbconnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnect =()=>{
-    mongoose.connect('mongodb+srv://22je0094:6fUKORytz4DEB9cr@cluster0.rz6z7c1.mongodb.net/').then(()=>{
+    mongoose.connect('mongodb+srv://22je0094:6fUKORytz4DEB9cr@cluster0.rz6z7c1.mongodb.net/Job_Seeking?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
         console.log("db Connected");
     })
     .catch((err)=>{
---
+++ frontend/src/components/Job/MyJobs.jsx
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../../index';
+import './MyJob.css';
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../../header';
+function MyJobs() {
+  const { isAuthorized, setIsAuthorized, user } = useContext(Context);
+  const [data, setData] = useState([]);
+  const navigateTo=useNavigate();
+  if(!isAuthorized){
+    navigateTo("/login");
+  }
+  useEffect(() => {
+    const getMyJobs = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/job/myjobs`, { withCredentials: true });
+        setData(response.data.myjobs);
+        console.log(response.data.myjobs);
+      } catch (error) {
+        console.error("Error fetching my jobs:", error);
+      }
+    };
+
+    getMyJobs();
+  }, []); 
+
+  const deletethejob = async (id) => {
+    try {
+      const {data}= await axios.delete(`${BASE_URL}/job/delete/${id}`, { withCredentials: true });
+      console.log(data);
+      toast.success(data.message);
+      window.location.reload();
+    } catch (error) {
+      console.error("Error deleting the job:", error);
+    }
+  };
+
+  console.log("Data in state:", data); 
+  return (
+    <div className='my-job'>
+      {data.map((job, index) => (
+        <div className="job-card" key={index}>
+          <div className="title">Title: {job.title}</div>
+          <div className="category">Category: {job.category}</div>
+          <div className="city">City: {job.city}</div>
+          <div className="country">Country: {job.country}</div>
+          <div className="location">Location: {job.location}</div>
+          <div className="description">Description: {job.description}</div>
+          {job.fixedSalary ? (
+            <div className="salary">Fixed Salary: {job.fixedSalary}</div>
+          ) : (
+            <div className="salary">
+              Salary
+              <div className="from">From: {job.SalaryFrom}</div>
+              <div className="to">To: {job.SalaryTo}</div>
+            </div>
+          )}
+              
+
+              <button onClick={()=>deletethejob(job._id)}>Delete</button>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MyJobs;
