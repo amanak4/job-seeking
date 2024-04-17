309188168cdd91eb7be585ce73dc886298580333
amanak7 <22je0094@iitism.ac.in>
2024-04-17 06:45:13 +0530

    k

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ dotenv.config({path:"./config/config.env"});
 
 app.use(
     cors({
-        origin:["https://job-seeking-three.vercel.app"],
+        origin:["https://job-seeking-three.vercel.app","https://job-seeking-7.onrender.com"],
         methods:["GET","POST","DELETE","PUT"],
         credentials:true,
     })
